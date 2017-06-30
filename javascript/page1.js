aaac18a51c528a49921aa7400ca684b87e167cf0
Charlotte Astle <charlotteastle@calvisitor-10-105-192-46.calvisitor.1918.berkeley.edu>
2017-06-30 11:44:33 -0700

    integrates new js files

---
+++ javascript/page1.js
@@ -0,0 +1,56 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBRke64vqW-1wHuxDisMWsBqzHlCNwAp8Y",
+    authDomain: "tindoor-9854b.firebaseapp.com",
+    databaseURL: "https://tindoor-9854b.firebaseio.com",
+    projectId: "tindoor-9854b",
+    storageBucket: "",
+    messagingSenderId: "331610741215"
+  };
+  firebase.initializeApp(config);
+
+
+ var jobTitle = response.results[i].jobtitle;
+  var locationCity = response.results[i].city;
+  var locationState = response. results[i].state;
+  var jobDescription = response.results[i].snippet;
+
+ // Grab the input from text
+  var jobTitle = $("#job-input").val().trim();
+  var location = $("#city-input").val().trim();
+  var state = $("#state-input").val().trim();
+
+ $("input[name='radius-5']").click(function(){
+    var radius = $("input[name='radius-5']:checked").val();
+   });
+  
+   $("input[name='radius-10']").click(function(){
+    var radius = $("input[name='radius-10']:checked").val();
+   });
+
+  $("input[name='radius-15']").click(function(){
+    var radius = $("input[name='radius-15']:checked").val();
+   });
+
+  $("input[name='radius-20']").click(function(){
+    var radius = $("input[name='radius-20']:checked").val();
+   });
+
+ // Select the year of experience
+
+ // selsect the location
+
+ //Select the radius in miles
+
+ var queryURL = "http://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+ jobTitle +"&l="+ location +"%2C+"+state+"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+
+
+ $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).done(function(response) {
+      console.log(response);
+       });
+  /*
+  1) Grab the input fro min
+  /*
