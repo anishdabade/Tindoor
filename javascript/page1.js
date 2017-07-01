5a9dc9d7da7d2a96b0af2447415ae0922e18bf2b
Charlotte Astle <charlotteastle@charlottes-mbp.attlocal.net>
2017-06-30 21:27:38 -0700

    links queryurl in js

--- javascript/page1.js
+++ javascript/page1.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBRke64vqW-1wHuxDisMWsBqzHlCNwAp8Y",
@@ -10,15 +12,7 @@
   firebase.initializeApp(config);
 
 
- var jobTitle = response.results[i].jobtitle;
-  var locationCity = response.results[i].city;
-  var locationState = response. results[i].state;
-  var jobDescription = response.results[i].snippet;
-
  // Grab the input from text
-  var jobTitle = $("#job-input").val().trim();
-  var location = $("#city-input").val().trim();
-  var state = $("#state-input").val().trim();
 
  $("input[name='radius-5']").click(function(){
     var radius = $("input[name='radius-5']:checked").val();
@@ -38,19 +32,73 @@
 
  // Select the year of experience
 
- // selsect the location
+ //Select the location
 
  //Select the radius in miles
 
- var queryURL = "http://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+ jobTitle +"&l="+ location +"%2C+"+state+"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+ function formatQueryString(str) {
+    var finalString;
+    var splitString = str.split(" ");
+    // console.log(splitString);
 
+    if (splitString.length > 1) {
+      finalString = splitString.join("+");
+      // console.log(splitString.length);
+    }
+
+    else {
+      finalString = str;
+        
+    }
+
+
+    return finalString;
+ };
+
+
+$("#submit-button").on("click", function(event){
+
+  var jobTitle = $("#job-input").val().trim();
+  title = formatQueryString(jobTitle);
+
+  var location = $("#city-input").val().trim();
+  city = formatQueryString(location);
+
+  var state = $("#state-input").val().trim();
+  state = formatQueryString(state);
+
+  // console.log(title);
+  // console.log(city);
+  // console.log(state);
+// "https://cors-anywhere.herokuapp.com/"
+
+var queryURL = "https://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+title+"&l="+city+"%2C+"+state+"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+console.log(queryURL);
+
+$('#test').html(queryURL);
 
  $.ajax({
     url: queryURL,
+    dataType: "jsonp",
     method: "GET"
     }).done(function(response) {
       console.log(response);
-       });
-  /*
-  1) Grab the input fro min
-  /*
+// console.log(jobTitle);
+// console.log(location);       
+
+for (var i=0; i<response.length; i++){
+
+  // var jobTitle = (response.results[i].jobtitle).val().trim();
+  // console.log("Job title: "+jobTitle);
+  // var location = (response.results[i].city).val().trim();
+  // var state = (response.results[i].state).val().trim();
+  // (response.results[i].snippet).val().trim();
+
+  // var locationCity = response.results[i].city;
+  // var locationState = response. results[i].state;
+  // var jobDescription = response.results[i].snippet;
+};
+
+  });
+});
+})
