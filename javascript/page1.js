eb916bab67254e9ba96dc9b6e0b9031845b7f66f
anishdabade <anish.dabade@gmail.com>
2017-07-01 19:02:59 -0700

    Modifing index,html and js files

--- javascript/page1.js
+++ javascript/page1.js
@@ -1,40 +1,39 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBRke64vqW-1wHuxDisMWsBqzHlCNwAp8Y",
-    authDomain: "tindoor-9854b.firebaseapp.com",
-    databaseURL: "https://tindoor-9854b.firebaseio.com",
-    projectId: "tindoor-9854b",
-    storageBucket: "",
-    messagingSenderId: "331610741215"
-  };
-  firebase.initializeApp(config);
+ //  var config = {
+ //    apiKey: "AIzaSyBRke64vqW-1wHuxDisMWsBqzHlCNwAp8Y",
+ //    authDomain: "tindoor-9854b.firebaseapp.com",
+ //    databaseURL: "https://tindoor-9854b.firebaseio.com",
+ //    projectId: "tindoor-9854b",
+ //    storageBucket: "",
+ //    messagingSenderId: "331610741215"
+ //  };
+ //  firebase.initializeApp(config);
 
 
- var jobTitle = response.results[i].jobtitle;
-  var locationCity = response.results[i].city;
-  var locationState = response. results[i].state;
-  var jobDescription = response.results[i].snippet;
+ // // var jobTitle = response.results[i].jobtitle;
+ // //  var locationCity = response.results[i].city;
+ // //  var locationState = response. results[i].state;
+ // //  var jobDescription = response.results[i].snippet;
 
  // Grab the input from text
-  var jobTitle = $("#job-input").val().trim();
-  var location = $("#city-input").val().trim();
-  var state = $("#state-input").val().trim();
-
- $("input[name='radius-5']").click(function(){
-    var radius = $("input[name='radius-5']:checked").val();
-   });
   
-   $("input[name='radius-10']").click(function(){
-    var radius = $("input[name='radius-10']:checked").val();
-   });
 
-  $("input[name='radius-15']").click(function(){
-    var radius = $("input[name='radius-15']:checked").val();
-   });
 
-  $("input[name='radius-20']").click(function(){
-    var radius = $("input[name='radius-20']:checked").val();
-   });
+ // $("input[name='radius-5']").on("click",function(){
+ //    var radius = $("input[name='radius-5']:checked").val();
+ //   });
+  
+ //   $("input[name='radius-10']").on("click",function(){
+ //    var radius = $("input[name='radius-10']:checked").val();
+ //   });
+
+ //  $("input[name='radius-15']").on("click",function(){
+ //    var radius = $("input[name='radius-15']:checked").val();
+ //   });
+
+ //  $("input[name='radius-20']").on("click",function(){
+ //    var radius = $("input[name='radius-20']:checked").val();
+ //   });
 
  // Select the year of experience
 
@@ -42,15 +41,33 @@
 
  //Select the radius in miles
 
- var queryURL = "http://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+ jobTitle +"&l="+ location +"%2C+"+state+"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+ $("#submit-button").on("click",function() {
+
+  var jobTitle = $("#job-input").val().trim();
+  var location = $("#city-input").val().trim();
+  var state = $("#state-input").val().trim();
+
+  console.log(jobTitle);
+  console.log(location);
+  console.log(state);
+  var queryURL = "https://cors-anywhere.herokuapp.com/" + "https://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+ jobTitle +"&l="+ location +"%2C+"+ state +"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json";
+var queryURL="https://cors-anywhere.herokuapp.com/" + "http://api.indeed.com/ads/apisearch?publisher=1665103808901378&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json";
+  
+
+console.log(queryURL);
 
 
- $.ajax({
+ // $(document).ready(function(){
+
+  
+    $.ajax({
     url: queryURL,
     method: "GET"
     }).done(function(response) {
       console.log(response);
        });
-  /*
-  1) Grab the input fro min
-  /*
+   // });
+  
+  
+   });
+ 
