60221209842bddb780766b13173b212cd10d8b20
Charlotte Astle <charlotteastle@calvisitor-10-105-194-173.calvisitor.1918.berkeley.edu>
2017-07-05 09:44:48 -0700

    map div

--- javascript/page1.js
+++ javascript/page1.js
@@ -14,43 +14,37 @@ $(document).ready(function(){
 
  // Grab the input from text
 
- $("input[name='radius-5']").click(function(){
-    var radius = $("input[name='radius-5']:checked").val();
+ $("input[name='radius']").click(function(){
+    var radius = $("input[value='5']:checked").val();
    });
   
-   $("input[name='radius-10']").click(function(){
-    var radius = $("input[name='radius-10']:checked").val();
+   $("input[name='radius']").click(function(){
+    var radius = $("input[value='10']:checked").val();
    });
 
-  $("input[name='radius-15']").click(function(){
-    var radius = $("input[name='radius-15']:checked").val();
+  $("input[name='radius']").click(function(){
+    var radius = $("input[value='15']:checked").val();
    });
 
-  $("input[name='radius-20']").click(function(){
-    var radius = $("input[name='radius-20']:checked").val();
+  $("input[name='radius']").click(function(){
+    var radius = $("input[value='20']:checked").val();
    });
 
- // Select the year of experience
 
- //Select the location
-
- //Select the radius in miles
 
  function formatQueryString(str) {
     var finalString;
     var splitString = str.split(" ");
-    // console.log(splitString);
+      // console.log(splitString);
 
     if (splitString.length > 1) {
       finalString = splitString.join("+");
-      // console.log(splitString.length);
+        // console.log(splitString.length);
     }
 
     else {
       finalString = str;
-        
     }
-
 
     return finalString;
  };
@@ -73,21 +67,50 @@ $("#submit-button").on("click", function(event){
 // "https://cors-anywhere.herokuapp.com/"
 
 var queryURL = "https://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+title+"&l="+city+"%2C+"+state+"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
-console.log(queryURL);
-
-$('#test').html(queryURL);
+  console.log("Query URL: "+queryURL);
 
  $.ajax({
     url: queryURL,
     dataType: "jsonp",
     method: "GET"
     }).done(function(response) {
-      console.log(response);
-// console.log(jobTitle);
-// console.log(location);       
+    
 
 for (var i=0; i<response.length; i++){
 
+  var resultSection = $("<div>");
+  var url = response.results[i].url;
+  resultSection.addClass= ("text-center");
+  resultSection.attr("id", "result-" + resultCounter);
+  $("#result-1").append(resultSection);
+  
+  $("#result-" + resultCounter)
+    .append("<h2>" + 
+      resultCounter + "<strong> " + response.results[i].jobtitle 
+      + "</strong></h2>");
+  $("#result-" + resultCounter)
+    .append("<p>" + response.results[i].formattedLocation + "</p>");
+  $("#result-" + resultCounter)
+    .append("<h4>" + response.results[i].company + "</h4><br>");
+
+
+
+  var newSearch = {
+    job_title: response.results[i].jobtitle,
+    location: response.results[i].formattedLocation,
+    company: response.results[i].company,
+    latitude: response.results[i].latitude,
+    longitude: response.results[i].longitude,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP,
+  };
+
+  database.ref().push(newSearch);
+
+};
+
+//go to page 2 and display above results.
+  // $("#result-1").append(resultSection);
+  
   // var jobTitle = (response.results[i].jobtitle).val().trim();
   // console.log("Job title: "+jobTitle);
   // var location = (response.results[i].city).val().trim();
@@ -97,8 +120,8 @@ for (var i=0; i<response.length; i++){
   // var locationCity = response.results[i].city;
   // var locationState = response. results[i].state;
   // var jobDescription = response.results[i].snippet;
-};
 
-  });
 });
+});
+
 })
