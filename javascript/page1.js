267d7bdf89abe8332363377f26d16432d7b6a9ad
Austin Alexander Lee <austin.alexander.lee@gmail.com>
2017-07-05 10:02:13 -0700

    Adds Google Maps Function with Marker

--- javascript/page1.js
+++ javascript/page1.js
@@ -1,3 +1,5 @@
+
+
 // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBRke64vqW-1wHuxDisMWsBqzHlCNwAp8Y",
@@ -8,12 +10,6 @@
     messagingSenderId: "331610741215"
   };
   firebase.initializeApp(config);
-
-
- var jobTitle = response.results[i].jobtitle;
-  var locationCity = response.results[i].city;
-  var locationState = response. results[i].state;
-  var jobDescription = response.results[i].snippet;
 
  // Grab the input from text
   var jobTitle = $("#job-input").val().trim();
@@ -42,15 +38,23 @@
 
  //Select the radius in miles
 
- var queryURL = "http://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+ jobTitle +"&l="+ location +"%2C+"+state+"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+ var queryURL = "https://api.indeed.com/ads/apisearch?publisher=1665103808901378&q="+ jobTitle +"&l="+ location +"%2C+"+state+"&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
 
+ function runQuery(queryURL){
+
+  var jobTitle = response.results[i].jobtitle;
+  var locationCity = response.results[i].city;
+  var locationState = response. results[i].state;
+  var jobDescription = response.results[i].snippet;
 
  $.ajax({
     url: queryURL,
     method: "GET"
     }).done(function(response) {
-      console.log(response);
+      console.log("Response: " + response);
        });
-  /*
-  1) Grab the input fro min
-  /*
+    console.log("URL: " + queryURL);
+    console.log(jobTitle);
+    console.log(location)
+  }
+  
